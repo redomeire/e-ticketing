8144c25a26f0bba23dc5d6a6bf751df6da93b4da
Redomeire <redomeire2@gmail.com>
2026-03-27 01:28:12 +0700

    feat: add transpilePackages configuration for improved package handling

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: [
+    "jsdom",
+    "html-encoding-sniffer",
+    "@exodus/bytes"
+  ],
   images: {
     remotePatterns: [
       {
