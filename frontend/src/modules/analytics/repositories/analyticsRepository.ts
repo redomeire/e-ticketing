a0c438cbe5db4843d7e593a819c41b6433a7bf0a
Redomeire <redomeire2@gmail.com>
2026-03-30 13:39:01 +0700

    feat: add analytics data fetching and display in AdminDashboard

---
+++ frontend/src/modules/analytics/repositories/analyticsRepository.ts
@@ -0,0 +1,19 @@
+import { IHttpRequest, IHttpResponse } from "@/config/http";
+import { IAnalytics } from "../types/analytics";
+import api from "@/config/api";
+
+const getAnalytics = async (
+    req: IHttpRequest<{}>
+): Promise<IHttpResponse<IAnalytics>> => {
+    const response = await api.get(
+        '/admin/analytics',
+        req.options
+    );
+    return response.data;
+}
+
+const analyticsRepository = {
+    getAnalytics,
+};
+
+export default analyticsRepository;
