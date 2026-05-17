0c8b491148b60cbee3d44284098d81a9354ef2dd
Redomeire <redomeire2@gmail.com>
2026-05-17 22:27:18 +0700

    feat: add maintenance mode handling with proxy and UI updates

---
+++ frontend/src/modules/maintenance/proxy/maintenance.proxy.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { CustomProxy } from "@/proxy/chain";
+import { get } from "@vercel/edge-config";
+
+export default function withIsMaintenance(
+    proxy: CustomProxy
+): CustomProxy {
+    return async (
+        request: NextRequest,
+        event: NextFetchEvent,
+        response: NextResponse
+    ) => {
+        try {
+            const maintenance = await get('maintenance') as {
+                enabled: boolean;
+                durationMinutes: number;
+            };
+
+            if (maintenance.enabled) {
+                return NextResponse.rewrite(new URL("/maintenance", request.url));
+            }
+        } catch (error) {
+            console.error("Error fetching maintenance mode status:", error);
+        }
+        return await proxy(request, event, response);
+    };
+}
